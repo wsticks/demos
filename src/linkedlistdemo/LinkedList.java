8caa9e27907ef3b3e2dff7a27cc0382e2f52514e
wsticks <akannimuyiwa@gmail.com>
2020-03-31 13:08:11 +0100

    added a linked list data structure demo code

---
+++ src/linkedlistdemo/LinkedList.java
@@ -0,0 +1,12 @@
+package linkedlistdemo;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void show(){
+        System.out.println("print");
+    }
+}
